d55b7622a633b72ef40e0c945b4bbc03d31a3806
aw3s0me <akorovin00@gmail.com>
2017-01-06 12:11:50 +0100

    molecule test data + added property isDatatype to triple

--- src/main/java/HashPartitionerRun.scala
+++ src/main/java/HashPartitionerRun.scala
@@ -14,6 +14,25 @@ object HashPartitionerRun {
       appName("partition").
       getOrCreate()
 
+    // Fig 3: RDF-MT Creation MULDER Paper (a)
+    val moleculeInstances = Seq(
+      Molecule("s1", Map(
+        "p1" -> Triple("s1", "p1", "o1", false),
+        "p2" -> Triple("s1", "p2", "os2", false),
+        "p3" -> Triple("s1", "p3", "o3", false),
+        "p4" -> Triple("s1", "p4", "o4", false),
+        "p3" -> Triple("s1", "p3", "o5", false))),
+      Molecule("os2", Map(
+        "p3" -> Triple("os2", "p3", "o3", false),
+        "p6" -> Triple("os2", "p6", "s3", false))),
+      Molecule("s3", Map(
+        "p3" -> Triple("s3", "p3", "o6", false),
+        "p7" -> Triple("s3", "p7", "s4", false))),
+      Molecule("s4", Map(
+        "p3" -> Triple("s4", "p3", "o7", false),
+        "p8" -> Triple("s4", "p8", "8175133", true)))
+    )
+
     // TODO: 2. hash molecules thus create HashedMolecules (todo: write hash function)
     // TODO: 3. save to HDFS (dont forget to run hdfs)
   }
@@ -23,4 +42,7 @@ case class Molecule(private val subject: String, private val triples: Map[String
 
 case class HashedMolecule(private val hash: Long, private val subject: String, private val triples: Map[String, Triple])
 
-case class Triple(private val subject: String, private val property: String, private val obj: String)
+case class Triple( private val subject: String,
+                   private val property: String,
+                   private val obj: String,
+                   private val isDatatype: Boolean)
